83cca1778644fcd4bd5f708b7024251495fe1994
Akanksha Agroya <akankshaagroya@Seemas-MacBook-Air-2.local>
2025-08-12 10:46:31 +0530

    m2

---
+++ A_Ambitious_Kid.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+  int n; cin >> n;
+  vector<int> a(n);
+  for(int i = 0; i<n; i++){
+    cin >> a[i];
+  }
+  int mini = INT_MAX;
+  for(int i = 0; i <n; i++){
+    mini = min(mini, abs(a[i]));}
+  cout << mini << "\n";
+  return 0;
+    
+    
+}
