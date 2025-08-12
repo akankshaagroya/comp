a7f99ac099f0f864ca40be9058a9396d36e92789
Akanksha Agroya <akankshaagroyaa@gmail.com>
2025-08-12 14:50:36 +0530

    yotest

--- A_Ambitious_Kid.cpp
+++ A_Ambitious_Kid.cpp
@@ -1,16 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
-  int n; cin >> n;
+int main()
+{
+  int n;
+  cin >> n;
   vector<int> a(n);
-  for(int i = 0; i<n; i++){
+  for (int i = 0; i < n; i++)
+  {
     cin >> a[i];
   }
   int mini = INT_MAX;
-  for(int i = 0; i <n; i++){
-    mini = min(mini, abs(a[i]));}
+  for (int i = 0; i < n; i++)
+  {
+    mini = min(mini, abs(a[i]));
+  }
+
   cout << mini << "\n";
   return 0;
-    
-    
 }
