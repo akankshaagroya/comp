327bebf651bfa0da8b1b287473e19b5605bea8f1
Akanksha Agroya <akankshaagroyaa@gmail.com>
2025-08-21 22:22:41 +0530

    lol

--- C_1_The_Cunning_Seller_easy_version.cpp
+++ C_1_The_Cunning_Seller_easy_version.cpp
@@ -3,7 +3,26 @@ using namespace std;
 int main(){
   int t; cin >> t;
   while(t--){
-    int n; cin >> n;
+    long long n; cin >> n;
+    int x = 0;
+    long long ans=0;
+    long long p=1;
+    while(n>0){
+      int d = n %3;
+      long long cost;
+      if(x==0) {
+        cost=3;
+      }
+      else{
+        cost = 3*p + x * (p/3);
+        
+      }
+      ans +=  d * cost;
+      n = n/3;
+      x++;
+      p *= 3;
+    }
+    cout << ans << "\n";
     
   }
 }
